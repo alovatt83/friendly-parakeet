f9f082ae2c0848ee8666c36638d248b1ad8c8030
Allen <allenlovatt@gmail.com>
2020-12-20 09:34:13 -0500

    Finizing code

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,77 +3,75 @@ var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
 var numbers = "0123456789";
 var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-var passwordReturn = document.querySelector("#password");
-var generator = document.querySelector("#generate");
+var passwordReturn = document.querySelector ("#password");
+var generator = document.querySelector ("#generate");
 
 // Generate Function
-function createPassword() {
+function createPassword () {
 
 var password = "";
 var passwordSelect = "";
 
 //Character Length Selection
-var passwordLength = window.prompt("What length would you like your password to be?");
-    if(
+var passwordLength = window.prompt ("What length would you like your password to be?");
+    if (
 // Character Validity Checker
-    passwordLength >128 ||  passwordLength <8 ) {window.alert("Please enter a valid number, between 8 and 128.");
-    return;
-    }
+    passwordLength >128 ||  passwordLength <8 ) { window.alert ("Please enter a valid number, between 8 and 128.");
+    return;}
 // Include Number Selection
-var characterSettings0 = window.confirm("Include numbers?");
-    console.log(characterSettings0);
+var characterSettings0 = window.confirm ("Include numbers?");
+    console.log (characterSettings0);
 
-    if(characterSettings0) {passwordSelect += numbers;
-    } console.log(passwordSelect);
+    if (characterSettings0) {passwordSelect += numbers;} 
+    console.log (passwordSelect);
 // Include Special Character Selection      
-var characterSettings1 = window.confirm("Include special characters?");
-    console.log(characterSettings1);
+var characterSettings1 = window.confirm ("Include special characters?");
+    console.log (characterSettings1);
 
-    if(characterSettings1) {passwordSelect += special;
-    } console.log(passwordSelect);
+    if (characterSettings1) {passwordSelect += special;} 
+    console.log (passwordSelect);
 // Include Lowercase Letters Selection      
-var characterSettings2 = window.confirm("Include lowercase letters?");
-    console.log(characterSettings2);
 
-    if(characterSettings2) {passwordSelect += lowercase;
-    } console.log(passwordSelect);
+var characterSettings2 = window.confirm ("Include lowercase letters?");
+    console.log (characterSettings2);
+
+    if (characterSettings2) {passwordSelect += lowercase;} 
+    console.log (passwordSelect);
 // Include Uppercase Letters Selection      
 var characterSettings3 = window.confirm("Include uppercase letters??");
-    console.log(characterSettings3);
+    console.log (characterSettings3);
 
-    if(characterSettings3) {passwordSelect += uppercase;
-    } console.log(passwordSelect);
+    if (characterSettings3) {passwordSelect += uppercase;} 
+    console.log (passwordSelect);
 
 // Has the Minimum Criteria Been Met?
     
-    if (passwordSelect === "") {window.alert("You must choose at least one option of character types to include!");
-    }
+    if (passwordSelect === "") {window.alert("You must choose at least one option of character types to include!");}
 
 // Character Randomizer Functions
 
-    if(passwordLength >8 && passwordLength <128) {
-    for(var i = 0; i < passwordLength; i++) {
-    password += passwordSelect.charAt(Math.floor(Math.random() * passwordSelect.length));
-    };
+    if (passwordLength >8 && passwordLength <128) {
+    for (var i = 0; i < passwordLength; i++) {
+    password += passwordSelect.charAt(Math.floor(Math.random() * passwordSelect.length));};
               
 // place this generated password in the local storage
-    localStorage.setItem("password", password);
-var password = localStorage.getItem("password");
-    console.log(password);
+    localStorage.setItem ("password", password);
+var password = localStorage.getItem ("password");
+    console.log (password);
     return password;
     };
     };
 
 // Push Information Back to HTML Textbox
 
-    function displayPassword() {
-var password = createPassword();
-var passwordReturn = document.querySelector("#password");
+    function displayPassword () {
+var password = createPassword ();
+var passwordReturn = document.querySelector ("#password");
     passwordReturn.value = password;
     };
 
 // Link to Generator Element
-var generator = document.querySelector("#generate");
+var generator = document.querySelector ("#generate");
 
 // Event Listener to HTML Generate Button
     generator.addEventListener("click", displayPassword);
